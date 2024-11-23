3c029b9e78e978a4e83a4f5e57d4c08e1ceab674
JoaoPedro <jpmartinsf27@gmail.com>
2024-11-22 22:50:13 -0300

    [chore] Updated init file

--- core/__init__.py
+++ core/__init__.py
@@ -3,3 +3,4 @@ from core.graph_representations import AdjacencyList
 from core.graph_representations import AdjacencyMatrix
 from core.graph_metrics import GraphMetrics
 from core.graph_algorithms import GraphAlgorithms
+from core.graph_algorithms import GraphTraversal
